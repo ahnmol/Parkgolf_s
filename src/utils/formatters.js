7d776006c62fc817b85a01ed20046be24b7267b7
ahnmol <ahnmol33@gmail.com>
2025-02-05 13:28:06 +0900

    1

--- src/utils/formatters.js
+++
@@ -1,9 +0,0 @@
-export const formatFileName = (name, extension) => {
-  return name
-    ? `${name.replace(/\s+/g, '_')}.${extension}`
-    : `tournament_data.${extension}`;
-};
-
-export const sanitizeScore = (score) => {
-  return parseInt(score, 10) || 0;
-}; 
